6070ba8fbc8b17150ea6701a97a1a623c12d9151
Russell Thomas <iamgrimus@gmail.com>
2017-12-07 21:57:49 -0800

    initial commit

---
+++ js/main.js
@@ -0,0 +1,87 @@
+function renderHome() {
+  const renderString = `<div>
+  <h1>Hello, I'm Russell. A fullstack developer & social media avoider</h1>
+  <p>I am a web developer and recent graduate of the Front End Web Development Career Path bootcamp at Thinkful. I love
+  coding because solving problems and seeing ideas come to life on the page is supremely satisfying. I geek out on
+  higher order functions, finding and learning awesome libraries or frameworks, and getting front and back ends to
+  play nicely. When I'm not coding, I'm often found on Skype working with clients, curled up in a cozy chair reading,
+  hiking in a canyon, or upside down doing a headstand. I am a lifelong learner and my current learning projects
+  (beyond code!) include piano and writing my first book.</p>
+</div>`;
+  $('.contentBlock').html(renderString);
+}
+function renderAbout() {
+  const renderString = `<div>
+  <h1>Hello, I'm Russell. A fullstack developer & social media avoider</h1>
+  <p>I am a web developer and recent graduate of the Front End Web Development Career Path bootcamp at Thinkful. I love
+  coding because solving problems and seeing ideas come to life on the page is supremely satisfying. I geek out on
+  higher order functions, finding and learning awesome libraries or frameworks, and getting front and back ends to
+  play nicely. When I'm not coding, I'm often found on Skype working with clients, curled up in a cozy chair reading,
+  hiking in a canyon, or upside down doing a headstand. I am a lifelong learner and my current learning projects
+  (beyond code!) include piano and writing my first book.</p>
+</div>`;
+  $('.aboutRender').click(function() {
+    $('.contentBlock').html(renderString);
+    $('.hamburger').toggleClass('open');
+    $('.menu-overlay').toggleClass('open');
+  });
+}
+function renderProjects() {
+  const renderString = `<div><div class="two-col col-row clearfix">
+  <div class="col col-1-2">
+      <figure>
+          <img src="img/1-blubber-brothers.jpg" alt="">
+          <figcaption>
+              <h3>Blubber Brothers</h3>
+              <span>Burnt Toast Creative</span>
+              <a href="#" class="close-caption hidden">x</a>
+          </figcaption>
+      </figure>
+  </div>
+  <div class="col col-2-2">
+      <figure>
+          <img src="img/2-tits-and-wieners.jpg" alt="">
+          <figcaption>
+              <h3>Tits and Wieners</h3>
+              <span>Burnt Toast Creative</span>
+              <a href="#" class="close-caption hidden">x</a>
+          </figcaption>
+      </figure>
+  </div>
+</div>
+</div>`;
+  $('.projectRender').click(function() {
+    $('.contentBlock').html(renderString);
+    $('.hamburger').toggleClass('open');
+    $('.menu-overlay').toggleClass('open');
+  });
+}
+function renderContactInfo() {
+  const renderString = `<div>
+  <h1>incase you want to reach&nbsp;out</h1>
+  <p>conact me here and here and hopefully there are some cool logos or something.
+  </p>
+  <p>and a resume link</p>
+  <ul class="social-icons">
+  <li><a href="#"><i class="fa fa-twitter fa-3x" aria-hidden="true"></i></a></li>
+  <li><a href="#"><i class="fa fa-linkedin fa-3x" aria-hidden="true"></i></a></li>
+  <li><a href="#"><i class="fa fa-github fa-3x" aria-hidden="true"></i></a></li>
+  <li><a href="#"><i class="fa fa-envelope-o fa-3x" aria-hidden="true"></i></a></li>
+  </ul>
+</div>`;
+  $('.contactRender').click(function() {
+    $('.contentBlock').html(renderString);
+    $('.hamburger').toggleClass('open');
+    $('.menu-overlay').toggleClass('open');
+  });
+}
+$(document).ready(function() {
+  $('.hamburger').click(function() {
+    $(this).toggleClass('open');
+    $('.menu-overlay').toggleClass('open');
+  });
+  renderHome();
+  renderAbout();
+  renderProjects();
+  renderContactInfo();
+});
---
+++ readme.md
@@ -0,0 +1,30 @@
+# Personal Portfolio
+
+This is the personal portfolio for Russell Thomas. Thanks for checking things
+out.
+
+## Responsive
+
+The site is fully responsive and quickly adapts to all mobile, tablet, and
+desktop viewports.
+
+## Built With
+
+* [Javascript]
+* [jQuery]
+* [HTML5]
+* [CSS3]
+
+## Authors
+
+* **Iam Grimus**
+
+## License
+
+This project is licensed under the MIT License - see the
+[LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+
+* Doug Mason
+* The Thinkful community
