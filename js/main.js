017ee1c4647533d46c20120af346014e5bdc0ac7
Russell Thomas <iamgrimus@gmail.com>
2017-12-10 13:27:54 -0800

    content update site wide

--- js/main.js
+++ js/main.js
@@ -1,65 +1,66 @@
-function renderHome() {
-  const renderString = `<div>
-  <h1>Hello, I'm Russell. A fullstack developer & social media avoider</h1>
-  <p>I am a web developer and recent graduate of the Front End Web Development Career Path bootcamp at Thinkful. I love
-  coding because solving problems and seeing ideas come to life on the page is supremely satisfying. I geek out on
-  higher order functions, finding and learning awesome libraries or frameworks, and getting front and back ends to
-  play nicely. When I'm not coding, I'm often found on Skype working with clients, curled up in a cozy chair reading,
-  hiking in a canyon, or upside down doing a headstand. I am a lifelong learner and my current learning projects
-  (beyond code!) include piano and writing my first book.</p>
+const aboutRenderString = `<div>
+<h1>Hello, I'm Russell.</h1>
+<p>A lifelong learner, a full stack JavaScript and email developer, and graduate of the Full stack JavaScript Boot camp from Thinkful.</p>
+<p>I love the challenges of this industry and the constant learning it requires to stay relevant. At times it feels as if there is a theme guiding my career that continuously brings me back to the path of education. I enjoy learning new things and know the importance of constant personal and professional growth.</p>
+<p>On the weekends you can find me fighting my way through a book on JS, geeking out over higher order functions or gaming with friends. When I do step away from the computer, I attempt to leave the grid behind, exploring the forests of WA with my wife and pup, reconnecting with the natural world or biking with my dad until my legs cry&nbsp;uncle.</p>
 </div>`;
-  $('.contentBlock').html(renderString);
-}
-function renderAbout() {
-  const renderString = `<div>
-  <h1>Hello, I'm Russell. A fullstack developer & social media avoider</h1>
-  <p>I am a web developer and recent graduate of the Front End Web Development Career Path bootcamp at Thinkful. I love
-  coding because solving problems and seeing ideas come to life on the page is supremely satisfying. I geek out on
-  higher order functions, finding and learning awesome libraries or frameworks, and getting front and back ends to
-  play nicely. When I'm not coding, I'm often found on Skype working with clients, curled up in a cozy chair reading,
-  hiking in a canyon, or upside down doing a headstand. I am a lifelong learner and my current learning projects
-  (beyond code!) include piano and writing my first book.</p>
+
+const projectRenderString = `<div>
+  <section>
+    <figure>
+    <a href="https://battle-scratch.netlify.com/"><img src="img/battle-lg_680x462.png" alt="Battle Skratch: React app for DJs"></a>
+      <figcaption>
+        <p class="tech-stack">React, Redux, JavaScript, HTML5, CSS3, Node, Express, Mocha, Chai, MongoDB</p>
+      </figcaption>
+    </figure>
+    <div class="project-description">
+      <h2 class="battle-skratch">Battle Skratch</h2>
+      <p>Battle Skratch is a web app for turntablists of all skill levels. The goal is to promote the art of turntablism and the community of DJs through monthly friendly and informal video battles.</p>
+      <ul class="project-icons">
+      <li><a href="https://github.com/IamGrimUs/battle-skratch-client"><i class="fa fa-github fa-2x" aria-hidden="true"></i> APP</a></li>
+      <li><a href="https://github.com/IamGrimUs/battle-skratch-server"><i class="fa fa-github fa-2x" aria-hidden="true"></i> API</a></li>
+      <li><a href="https://battle-scratch.netlify.com/"><i class="fa  fa-external-link-square fa-2x" aria-hidden="true"></i> Netlify</a></li>
+      </ul>
+    <div>
+  </section>
+  <section class="project-container">
+    <figure>
+    <a href="https://agglomeration.herokuapp.com/login.html"><img src="img/agglomeration-lg_680x462.png" alt="Agglomeration: a contact organizer"></a>
+      <figcaption>
+        <p class="tech-stack">JavaScript, jQuery, HTML5, CSS3, Node, Express, Mocha, Chai, MongoDB</p>
+      </figcaption>
+    </figure>
+    <div class="project-description">
+      <h2 class="agglomeration">Agglomeration</h2>
+      <p>Agglomeration was created to combat the feeling of isolation when your new to an existing team or part of remote team. It provides easy access to search out team members for contact or learning a little bit about them.</p>
+      <ul class="project-icons">
+      <li><a href="https://github.com/IamGrimUs/agglomeration"><i class="fa fa-github fa-2x" aria-hidden="true"></i> APP & API</a></li>
+      <li><a href="https://agglomeration.herokuapp.com/login.html"><i class="fa  fa-external-link-square fa-2x" aria-hidden="true"></i> Heroku</a></li>
+      </ul>
+    <div>
+  </section>
+  <section class="project-container">
+    <figure>
+    <a href="https://iamgrimus.github.io/ataraxia/"><img src="img/ataraxia-lg_680x462.png" alt="Ataraxia: a javascript slideshow created with the unsplash api."></a>
+      <figcaption>
+        <p class="tech-stack">JavaScript, jQuery, HTML5, CSS3</p>
+      </figcaption>
+    </figure>
+    <div class="project-description">
+      <h2 class="ataraxia">Ataraxia</h2>
+      <p>A small corner of cyberspace that was built to help you rest your mind. Take some time to breathe deep. Do some light stretching. Reclaim a small part of your day, and get lost in the beauty of the world through photography. Be inspired to embark on adventures waiting for you just beyond the glass.</p>
+      <p>The project was built by accessing the unsplash API to display a random set of beautiful imagery.</p>
+      <ul class="project-icons">
+        <li><a href="https://github.com/IamGrimUs/ataraxia"><i class="fa fa-github fa-2x" aria-hidden="true"></i> APP</a></li>
+        <li><a href="https://iamgrimus.github.io/ataraxia/"><i class="fa  fa-external-link-square fa-2x" aria-hidden="true"></i> GitHub pages</a></li>
+      </ul>
+    <div>
+  </section>
 </div>`;
-  $('.aboutRender').click(function() {
-    $('.contentBlock').html(renderString);
-    $('.hamburger').toggleClass('open');
-    $('.menu-overlay').toggleClass('open');
-  });
-}
-function renderProjects() {
-  const renderString = `<div><div class="two-col col-row clearfix">
-  <div class="col col-1-2">
-      <figure>
-          <img src="img/1-blubber-brothers.jpg" alt="">
-          <figcaption>
-              <h3>Blubber Brothers</h3>
-              <span>Burnt Toast Creative</span>
-              <a href="#" class="close-caption hidden">x</a>
-          </figcaption>
-      </figure>
-  </div>
-  <div class="col col-2-2">
-      <figure>
-          <img src="img/2-tits-and-wieners.jpg" alt="">
-          <figcaption>
-              <h3>Tits and Wieners</h3>
-              <span>Burnt Toast Creative</span>
-              <a href="#" class="close-caption hidden">x</a>
-          </figcaption>
-      </figure>
-  </div>
-</div>
-</div>`;
-  $('.projectRender').click(function() {
-    $('.contentBlock').html(renderString);
-    $('.hamburger').toggleClass('open');
-    $('.menu-overlay').toggleClass('open');
-  });
-}
-function renderContactInfo() {
-  const renderString = `<div>
-  <h1>Incase you want to reach&nbsp;out</h1>
-  <p>Please don't hesitate to contact me. I am open to any and all opertunities. My goal is to never stop learning from others, and to teach everything I know to anyone interested in learning.
+
+const contactRenderString = `<div>
+  <h1>Let's talk</h1>
+  <p>Please don't hesitate to contact me. I am open to any and all opportunities. My goal is to be a better person everyday. How do I achomplish the goal? By constantly learning from those around me, and by teach everything I know to those interested in learning.
   </p>
   <ul class="social-icons">
   <li><a href="https://twitter.com/IamGrimus"><i class="fa fa-twitter fa-3x" aria-hidden="true"></i></a></li>
@@ -68,19 +69,43 @@ function renderContactInfo() {
   <li><a href="mailto:iamgrimus@gmail.com?Subject=Good afternoon sir."><i class="fa fa-envelope-o fa-3x" aria-hidden="true"></i></a></li>
   </ul>
 </div>`;
-  $('.contactRender').click(function() {
-    $('.contentBlock').html(renderString);
+
+function homePageLoadAbout() {
+  $('.contentBlock').html(aboutRenderString);
+}
+function menuActions() {
+  $('.aboutRender').click(function() {
+    $('.contentBlock').html(aboutRenderString);
     $('.hamburger').toggleClass('open');
     $('.menu-overlay').toggleClass('open');
+    $('.closedAction').toggleClass('closed-menu');
+    $('.closedAction').toggleClass('open-menu');
+  });
+
+  $('.projectRender').click(function() {
+    $('.contentBlock').html(projectRenderString);
+    $('.hamburger').toggleClass('open');
+    $('.menu-overlay').toggleClass('open');
+    $('.closedAction').toggleClass('closed-menu');
+    $('.closedAction').toggleClass('open-menu');
+  });
+
+  $('.contactRender').click(function() {
+    $('.contentBlock').html(contactRenderString);
+    $('.hamburger').toggleClass('open');
+    $('.menu-overlay').toggleClass('open');
+    $('.closedAction').toggleClass('closed-menu');
+    $('.closedAction').toggleClass('open-menu');
   });
 }
+
 $(document).ready(function() {
+  homePageLoadAbout();
   $('.hamburger').click(function() {
     $(this).toggleClass('open');
     $('.menu-overlay').toggleClass('open');
+    $('.closedAction').toggleClass('closed-menu');
+    $('.closedAction').toggleClass('open-menu');
   });
-  renderHome();
-  renderAbout();
-  renderProjects();
-  renderContactInfo();
+  menuActions();
 });
